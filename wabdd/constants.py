cea9a61d02561a9873fad1aed84e18c2b7aec989
dhgatjeye <195902293+dhgatjeye@users.noreply.github.com>
2025-06-27 23:05:35 +0300

    Revert "added new feature"
    This reverts commit d522090c5dce65d1aa0969e169b425ac7da351a3.

--- wabdd/constants.py
+++ wabdd/constants.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pathlib
-import platform
 from . import __version__
 
 BACKUP_FOLDER = "backups"
